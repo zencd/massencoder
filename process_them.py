70a7cd43f499ed707a592ca9ae8ff5b28229ade6
zzz <z@z.com>
2025-10-14 15:29:11 +0300

    ok

--- process_them.py
+++ process_them.py
@@ -16,7 +16,7 @@ from wakepy.modes import keep
 from utils import create_dirs_for_file, PersistentList, hms, dhms, bell, is_same_volume
 
 MAX_WORKERS = 1
-BASE_DIR = Path.home() / "Downloads"
+BASE_DIR = Path.home() / 'Downloads'
 OUT_DIR = BASE_DIR / 'reenc-done-output'
 PROCESSED_INPUT_DIR = BASE_DIR / 'reenc-done-input'
 TMP_OUT_DIR = BASE_DIR / 'reenc-work'
