798e5d0a8b16ecff9baa58d7115532cacd4d0b5c
zzz <z@z.com>
2025-11-07 23:29:26 +0300

    ok

--- helper.py
+++ helper.py
@@ -4,6 +4,9 @@ import subprocess
 from pathlib import Path
 import shlex
 
+from utils import PersistentList
+
+
 def log(s, do_print=False):
     with open('log.txt', 'a', encoding='utf-8') as f:
         f.write(f'{s}\n')
@@ -21,16 +24,41 @@ def get_video_meta(video: Path):
     # log(f'Exec: {shlex.join(args)}')
     res = subprocess.run(args, stdout=subprocess.PIPE)
     rc = res.returncode & 0xFF
-    assert rc == 0, f'Ffprobe failed for: {video}'
+    assert rc == 0, f'Ffprobe failed with {rc} for: {video}'
     text = res.stdout.decode('utf-8')
     j = json.loads(text)
-    f = j['format']
+    fmt = j['format']
     videos = list(filter(lambda x: x['codec_type'] == 'video', j['streams']))
     audios = list(filter(lambda x: x['codec_type'] == 'audio', j['streams']))
-    return f, videos, audios
+    subtitles = list(filter(lambda x: x['codec_type'] == 'subtitle', j['streams']))
+    others = list(filter(lambda x: x['codec_type'] not in {'video', 'audio', 'subtitle'}, j['streams']))
+    return fmt, videos, audios, subtitles, others
 
 
 def calc_fps(video_meta: dict):
     r_frame_rate: str = video_meta['r_frame_rate']
     slash = r_frame_rate.index('/')
     return float(r_frame_rate[0:slash]) / float(r_frame_rate[slash + 1:])
+
+
+def _main():
+    que = PersistentList('list-que.txt')
+    for f in que.lines:
+        path = Path(f)
+        if not path.exists() or path.stem.startswith('._'):
+            continue
+        fmt, videos, audios, subtitles, others = get_video_meta(path)
+        if subtitles or others:
+            print(f'{f}')
+            for s in videos:
+                print(f'  VIDEO {s['index']}')
+            for s in audios:
+                print(f'  AUDIO {s['index']}')
+            for s in subtitles:
+                print(f'  SUB   {s['index']}')
+            for s in others:
+                print(f'  OTHER {s['index']}')
+
+
+if __name__ == '__main__':
+    _main()
